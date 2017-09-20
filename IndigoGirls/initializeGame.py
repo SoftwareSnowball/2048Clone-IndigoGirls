18e0647f948d2cef84072b3ea345347f261a5896
InperialHedgehog <ImperialHedgehog@gmail.com>
2017-09-20 13:51:41 -0500

    Initial Commit

---
+++ IndigoGirls/initializeGame.py
@@ -0,0 +1,15 @@
+'''
+    Created on September 20, 2017
+
+    @Author: James Malloy
+
+    Purpose of File:
+        To implement the initalizeGame function
+
+    todo: finish description
+
+'''
+
+
+def initializeGame(messageDictionary):
+    pass;
---
+++ IndigoGirls/test/initializeGameTest.py
@@ -0,0 +1,26 @@
+"""
+    Created on September 20, 2017
+
+    @Author: James Malloy
+
+    This file contains unit test pertaining to the initializeGame function
+"""
+
+from unittest import TestCase
+import IndigoGirls.initializeGame as initializeGame
+
+
+#----------------------------------
+#Acceptance Tests:
+#
+#
+#
+#
+#
+#
+
+class InitializeGameTest(TestCase):
+
+    def testThis(self):
+        pass
+
