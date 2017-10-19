341b79442975529d33db3bfbfada34f279f43d14
James <jem0044@tigermail.auburn.edu>
2017-10-19 10:52:37 -0500

    Additional work done on swipe Swipe is now able to validate inputs

--- IndigoGirls/swipe.py
+++ IndigoGirls/swipe.py
@@ -5,9 +5,71 @@
     @Author: James Malloy
 
     Purpose of File:
-        To hold the swipe function
+        Holds swipe function which performs the swipe task in the game
 
 '''
 
-def swipe():
-    pass
+from IndigoGirls.ElementLine import ElementLine
+
+
+def swipe(input):
+
+    #Function is used to simplify the validity check for both rowCount and columnCount
+    #This function is also declared in initializeGame.
+    #TODO: refactor this into its own file
+    def isNotValidNumber(x):
+        if (type(x) is not int):
+            return True
+
+        if ((x <= 1) or (x > 100)):
+            return True
+
+        return False
+
+
+
+    board = input["board"]
+    direction = input["direction"]
+
+    errorMessage = "Error: "
+    error = False
+
+    if (type(direction) is not str):
+        errorMessage += "Direction must be string. "
+        error = True
+
+    if (direction is not "up" and direction is not "down" and direction is not "left" and direction is not "right"):
+        errorMessage += "Direction not recognized. "
+        error = True
+
+    if (isNotValidNumber(board["rowCount"])):
+        errorMessage += "rowCount is invalid. "
+        error = True
+
+    if (isNotValidNumber((board["columnCount"]))):
+        errorMessage += "columnCount is invalid. "
+        error = True
+
+    if (error):
+        return {"gameStatus": errorMessage}
+
+    # I don't want this code to run if rowCount or columnCount is not valid because it could cause an exception
+    if (len(board["grid"]) != board["rowCount"] * board["columnCount"]):
+        errorMessage += "Grid size is invalid. "
+        return {"gameStatus": errorMessage}
+
+    # At this point everything should be valid.
+
+
+    elementLines = []
+
+    elementLines.append(ElementLine(board, direction, 0))
+    elementLines.append(ElementLine(board, direction, 1))
+
+    elementLine = elementLines[0]
+    elementLine.setElement(0, 0)
+    elementLine.setElement(1, 1)
+
+    answer = {"board": board}
+
+    return answer
--- IndigoGirls/test/swipeTest.py
+++ IndigoGirls/test/swipeTest.py
@@ -39,7 +39,11 @@ from IndigoGirls.swipe import swipe
 
 class SwipeTest(TestCase):
 
-    def test_swipeValidate01(self):
+
+
+    #Invalid input tests
+
+    def test_swipe_ValidateDirection01(self):
         columnCount = 2
         rowCount = 2
         grid = [0] * 4
@@ -50,7 +54,129 @@ class SwipeTest(TestCase):
 
         output = swipe(input)
 
-        self.assertEqual(output["gamestatus"], "Error: Direction must be string")
+        self.assertTrue("Direction must be string" in output["gameStatus"])
 
 
 
+    def test_swipe_ValidateDirection02(self):
+        columnCount = 2
+        rowCount = 2
+        grid = [0] * 4
+        direction = "north"
+
+        board = {"columnCount": columnCount, "rowCount" :rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("Direction not recognized" in output["gameStatus"])
+
+
+    def test_swipe_ValidateRow01(self):
+        columnCount = 2
+        rowCount = 1
+        grid = [0] * 2
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount" :rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("rowCount is invalid" in output["gameStatus"])
+
+    def test_swipe_ValidateRow02(self):
+        columnCount = 2
+        rowCount = 101
+        grid = [0] * 2
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("rowCount is invalid" in output["gameStatus"])
+
+
+    def test_swipe_ValidateColumn01(self):
+        columnCount = 1
+        rowCount = 2
+        grid = [0] * 2
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("columnCount is invalid" in output["gameStatus"])
+
+    def test_swipe_ValidateColumn02(self):
+        columnCount = 101
+        rowCount = 2
+        grid = [0] * 2
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("columnCount is invalid" in output["gameStatus"])
+
+    def test_swipe_ValidateColumn03(self):
+        columnCount = "5"
+        rowCount = 2
+        grid = [0] * 2
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("columnCount is invalid" in output["gameStatus"])
+
+
+    def test_swipe_ValidateGrid01(self):
+        columnCount = 3
+        rowCount = 2
+        grid = [0] * (3*2 - 1)
+        direction = "up"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertTrue("Grid size is invalid" in output["gameStatus"])
+
+
+    #Functionality Tests
+
+    def test_swipe_FunctionalTest01(self):
+        columnCount = 2
+        rowCount = 2
+        grid = [1, 0, 0, 0]
+        direction = "right"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertEqual([0,1,0,0], output["board"]["grid"])
+
+    def test_swipe_FunctionalTest02(self):
+        columnCount = 2
+        rowCount = 2
+        grid = [0, 1, 0, 0]
+        direction = "left"
+
+        board = {"columnCount": columnCount, "rowCount": rowCount, "grid": grid}
+        input = {"board": board, "direction": direction}
+
+        output = swipe(input)
+
+        self.assertEqual([1,0,0,0], output["board"]["grid"])
